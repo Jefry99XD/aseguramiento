1f8e69b864d2fbfea4523f29271b6a461466902a
Jefry99XD <jeffryfredo@gmail.com>
2023-11-15 19:35:42 -0600

    1b

--- 1b/1b.py
+++ 1b/1b.py
@@ -1,3 +1,4 @@
+from datetime import date
 # fecha = (year, month, day)
 def fecha_es_tupla(f):
     #Todas las fechas deben ser creadas como tuplas de tres números enteros positivos (ternas)
@@ -15,6 +16,8 @@ def fecha_es_valida(f):
         case 31:
             if f[1] == 1 or f[1] == 3 or f[1] == 5 or f[1] == 7 or f[1] == 8 or f[1] == 10 or f[1] == 12:
                 return True
+            elif f[1] == 4 or f[1] == 6 or f[1] == 9 or f[1] == 11:
+                return False
             elif f[1] < 13 and f[2] < 32:
                 return True
             else:
@@ -22,6 +25,8 @@ def fecha_es_valida(f):
         case 30:
             if f[1] == 4 or f[1] == 6 or f[1] == 9 or f[1] == 11:
                 return True
+            elif f[1] == 1 or f[1] == 3 or f[1] == 5 or f[1] == 7 or f[1] == 8 or f[1] == 10 or f[1] == 12:
+                return False
             elif f[1] < 13 and f[2] < 32:
                 return True
             else:
@@ -140,17 +145,75 @@ def ordinal_dia(f):
         case 2:
             return 31 + f[2]
 
+def dia_semana(f):
+    #
+    if not fecha_es_valida(f):
+        raise Exception("Fecha no valida")
+    return int((f[2] +  f[0] + (f[0]/4) + (31*f[1]/12)) % 7)
 
+def fecha_futura(f, n):
+    if not fecha_es_valida(f):
+        raise Exception("Fecha no valida")
+    if n < 0:
+        raise Exception("n no valido")
+    if n ==0:
+        return f
+    fechaFutura = f
+    for i in range(0, n):
+        fechaFutura = dia_siguiente(fechaFutura)
+    return fechaFutura
+    
+def dias_entre(f1, f2):
+    if not fecha_es_valida(f1) and not fecha_es_valida(f2):
+        raise Exception("Fecha no valida")
+    dias = 0
+    copy = f1
+    if f1 < f2:
+        while copy != f2:
+            copy = dia_siguiente(copy)
+            dias+=1
+    else:
+        copy2 = f2
+        while copy2 != f1:
+            copy2 = dia_siguiente(copy2)
+            dias+=1
+    return dias
+    
+    
 
-a = (2023, 11, 11)
+def edad_al(f1, f2):
+    if not fecha_es_valida(f1) and not fecha_es_valida(f2):
+        raise Exception("Fecha no valida")
+    if f1[0] > f2[0]:
+         raise Exception("Fecha no valida")
+    edad = [abs(f1[0]-f2[0]), abs(f1[1]-f2[1]), abs(f1[2]-f2[2])]
+    if edad[1] == 0:
+        if edad[2] > 0:
+            edad[1]=f1[1]
+            edad[0] -= 1
+    return tuple(edad)
+    
+def fecha_hoy():
+    f = (date.today().year, date.today().month, date.today().day)
+    return f
+
+def edad_hoy(f):
+    if not fecha_es_valida(f):
+        raise Exception("Fecha no valida")
+    return edad_al(f, fecha_hoy())
+
+a = (2020, 4, 22)
+nac = (1999, 11,27)
 print(a)
-print("La fecha es tupla?")
-print(fecha_es_tupla(a))
-print("La fecha es bisiesta?")
-print(bisiesto(a))
-print("La fecha es valida?")
-print(fecha_es_valida(a))
-print("El dia siguiente es")
-print(dia_siguiente(a))
-print("el numero del dia en el año es?")
-print(ordinal_dia(a))
+print("nacimiento prueba", nac)
+print("dia semana ", dia_semana(fecha_hoy()))
+print("fecha futura", fecha_futura(fecha_hoy(), 41))
+print("dias entre", a, " ", fecha_hoy(), "  ", dias_entre(a, fecha_hoy()))
+print("edad_al", edad_al((1999, 11,27), (2027,5,16)))
+print("fecha hoy", fecha_hoy()) 
+print("edad hoy", edad_hoy((1999, 11,27)))
+
+
+
+
+    
